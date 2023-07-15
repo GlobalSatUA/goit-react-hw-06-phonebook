6f582768e1c40a68605203774e96e60edd3ce3fb
GlobalSatUA <alex.trenbach@gmail.com>
2023-07-15 16:42:39 +0300

    work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import { addContact, deleteContact, updateFilter } from '../redux/contactsSlice';
+import ls from 'local-storage';
+import { nanoid } from 'nanoid';
+
+const App = () => {
+  const contacts = useSelector((state) => state.contacts);
+  const filter = useSelector((state) => state.filter);
+  const dispatch = useDispatch();
+  
+
+  useEffect(() => {
+    ls.set('contacts', contacts);
+  }, [contacts]);
+
+  const handleAddContact = (name, number) => {
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+    dispatch(addContact(newContact));
+  };
+
+  const handleDeleteContact = (id) => {
+    dispatch(deleteContact(id));
+  };
+
+  const handleFilterChange = (event) => {
+    dispatch(updateFilter(event.target.value));
+  };
+
+  const filteredContacts = filter ? contacts.filter((contact) =>
+  contact.name.toLowerCase().includes(filter.toLowerCase())
+) : contacts;
+
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div style={{ maxWidth: '250px', padding: '20px' }}>
+      <h1 style={{ marginBottom: '20px' }}>Phonebook</h1>
+      <ContactForm contacts={contacts} onAddContact={handleAddContact} />
+
+      <h2 style={{ marginTop: '40px' }}>Contacts</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ContactList contacts={filteredContacts} onDeleteContact={handleDeleteContact} />
     </div>
   );
 };
+
+export default App;
